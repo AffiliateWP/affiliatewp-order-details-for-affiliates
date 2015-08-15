2df013466046e428faf0f6213e07d8db521f91f3
Andrew Munro <andrew@sumobi.com>
2015-08-15 17:57:52 +1200

    Fix #7

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -1,7 +1,7 @@
 <?php
 
 class AffiliateWP_Order_Details_For_Affiliates_Admin {
-	
+
 	public function __construct() {
 		// update the affiliate
 		add_action( 'affwp_update_affiliate', array( $this, 'update_affiliate' ), 0 );
@@ -10,7 +10,7 @@ class AffiliateWP_Order_Details_For_Affiliates_Admin {
 		add_action( 'affwp_edit_affiliate_bottom', array( $this, 'admin_field' ) );
 
 		// add global option for access to order details
-		add_filter( 'affwp_settings_misc', array( $this, 'order_details_access' ) );
+		add_filter( 'affwp_settings_integrations', array( $this, 'order_details_access' ) );
 	}
 
 	/**
@@ -37,7 +37,7 @@ class AffiliateWP_Order_Details_For_Affiliates_Admin {
 		} else {
 			delete_user_meta( affwp_get_affiliate_user_id( $data['affiliate_id'] ), 'affwp_order_details_access' );
 		}
-		
+
 	}
 
 	/**
@@ -69,7 +69,7 @@ class AffiliateWP_Order_Details_For_Affiliates_Admin {
 			</tr>
 		</table>
 
-	<?php		
+	<?php
 	}
 
 	/**
@@ -80,7 +80,7 @@ class AffiliateWP_Order_Details_For_Affiliates_Admin {
 	 * @return boolean
 	 */
 	public function order_details_access( $fields ) {
-		
+
 		$fields['order_details_access'] = array(
 			'name' => __( 'Allow Global Access To Order Details', 'affiliatewp-order-details-for-affiliates' ),
 			'desc' => __( 'Check this box if you would like all affiliates to have access to order details.', 'affiliatewp-order-details-for-affiliates' ),
