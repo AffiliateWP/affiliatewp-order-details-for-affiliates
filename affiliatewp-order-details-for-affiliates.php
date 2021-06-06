5b17c53f320d738f79b80bcbef6420821f979f83
Drew Jaynes <drew@affiliatewp.com>
2021-06-06 02:20:26 -0700

    Create new main plugin file. #77

---
+++ affiliatewp-order-details-for-affiliates.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Order Details For Affiliates
+ * Plugin URI: https://affiliatewp.com/addons/order-details-affiliates/
+ * Description: Share customer purchase information with the affiliate who referred them.
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Version: 1.1.6
+ * Text Domain: affiliatewp-order-details-for-affiliates
+ *
+ * AffiliateWP is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * AffiliateWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Order Details For Affiliates
+ * @category Core
+ * @author AffiliateWP
+ * @version 1.1.6
+ */
+
