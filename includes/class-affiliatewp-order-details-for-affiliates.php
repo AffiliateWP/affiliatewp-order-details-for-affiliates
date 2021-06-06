e5e8300d7e84f2fbab668664e8b57e3c20bec8d2
Drew Jaynes <drew@affiliatewp.com>
2021-06-06 02:20:50 -0700

    Add a new file header to class-affiliatewp-order-details-for-affiliates.php. #77

--- includes/class-affiliatewp-order-details-for-affiliates.php
+++ includes/class-affiliatewp-order-details-for-affiliates.php
@@ -1,35 +1,19 @@
 <?php
 /**
- * Plugin Name: AffiliateWP - Order Details For Affiliates
- * Plugin URI: https://affiliatewp.com/addons/order-details-affiliates/
- * Description: Share customer purchase information with the affiliate who referred them.
- * Author: Sandhills Development, LLC
- * Author URI: https://sandhillsdev.com
- * Version: 1.1.6
- * Text Domain: affiliatewp-order-details-for-affiliates
+ * Core: Plugin Bootstrap
  *
- * AffiliateWP is distributed under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * AffiliateWP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
- *
- * @package Order Details For Affiliates
- * @category Core
- * @author AffiliateWP
- * @version 1.1.6
+ * @package     AffiliateWP Order Details for Affiliates
+ * @subpackage  Core
+ * @copyright   Copyright (c) 2021, Sandhills Development, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.2
  */
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
 /**
  * Main AffiliateWP_Order_Details_For_Affiliates class.
  *
