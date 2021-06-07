5258bcc521f9701807c69a4cddc5de7816a66970
Drew Jaynes <drew@affiliatewp.com>
2021-06-06 17:11:45 -0700

    Set up the new main plugin file from the plugin template. #77

--- affiliatewp-order-details-for-affiliates.php
+++ affiliatewp-order-details-for-affiliates.php
@@ -26,3 +26,130 @@
  * @version 1.1.6
  */
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'AffiliateWP_Requirements_Check' ) ) {
+	require_once dirname( __FILE__ ) . '/includes/lib/affwp/class-affiliatewp-requirements-check.php';
+}
+
+/**
+ * Class used to check requirements for and bootstrap the plugin.
+ *
+ * @since 1.2
+ *
+ * @see Affiliate_WP_Requirements_Check
+ */
+class AffiliateWP_ODFA_Requirements_Check extends AffiliateWP_Requirements_Check {
+
+	/**
+	 * Plugin slug.
+	 *
+	 * @since 1.2
+	 * @var   string
+	 */
+	protected $slug = 'affiliatewp-order-details-for-affiliates';
+
+	/**
+	 * Bootstrap everything.
+	 *
+	 * @since 1.2
+	 */
+	public function bootstrap() {
+		if ( ! class_exists( 'Affiliate_WP' ) ) {
+
+			if ( ! class_exists( 'AffiliateWP_Activation' ) ) {
+				require_once 'includes/lib/affwp/class-affiliatewp-activation.php';
+			}
+
+			// AffiliateWP activation
+			if ( ! class_exists( 'Affiliate_WP' ) ) {
+				$activation = new AffiliateWP_Activation( plugin_dir_path( __FILE__ ), basename( __FILE__ ) );
+				$activation = $activation->run();
+			}
+		} else {
+			\AffiliateWP_Order_Details_For_Affiliates::instance( __FILE__ );
+		}
+	}
+
+	/**
+	 * Loads the add-on.
+	 *
+	 * @since 1.2
+	 */
+	protected function load() {
+		// Maybe include the bundled bootstrapper.
+		if ( ! class_exists( 'AffiliateWP_Order_Details_For_Affiliates' ) ) {
+			require_once dirname( __FILE__ ) . '/includes/class-affiliatewp-order-details-for-affiliates.php';
+		}
+
+		// Maybe hook-in the bootstrapper.
+		if ( class_exists( 'AffiliateWP_Order_Details_For_Affiliates' ) ) {
+
+			$affwp_version = get_option( 'affwp_version' );
+
+			if ( version_compare( $affwp_version, '2.7', '>=' ) ) {
+				add_action( 'affwp_plugins_loaded', array( $this, 'bootstrap' ), 100 );
+			} else {
+				add_action( 'plugins_loaded', array( $this, 'bootstrap' ), 100 );
+			}
+
+			// Register the activation hook.
+			register_activation_hook( __FILE__, array( $this, 'install' ) );
+		}
+	}
+
+	/**
+	 * Install, usually on an activation hook.
+	 *
+	 * @since 1.2
+	 */
+	public function install() {
+		// Bootstrap to include all of the necessary files
+		$this->bootstrap();
+
+		if ( defined( 'AFFWP_ODFA_VERSION' ) ) {
+			update_option( 'affwp_odfa_version', AFFWP_ODFA_VERSION );
+		}
+	}
+
+	/**
+	 * Plugin-specific aria label text to describe the requirements link.
+	 *
+	 * @since 1.2
+	 *
+	 * @return string Aria label text.
+	 */
+	protected function unmet_requirements_label() {
+		return esc_html__( 'AffiliateWP - Order Details for Affiliates Requirements', 'affiliatewp-order-details-for-affiliates' );
+	}
+
+	/**
+	 * Plugin-specific text used in CSS to identify attribute IDs and classes.
+	 *
+	 * @since 1.2
+	 *
+	 * @return string CSS selector.
+	 */
+	protected function unmet_requirements_name() {
+		return 'affiliatewp-plugin-template-requirements';
+	}
+
+	/**
+	 * Plugin specific URL for an external requirements page.
+	 *
+	 * @since 1.2
+	 *
+	 * @return string Unmet requirements URL.
+	 */
+	protected function unmet_requirements_url() {
+		return 'https://docs.affiliatewp.com/article/2361-minimum-requirements-roadmaps';
+	}
+
+}
+
+$requirements = new AffiliateWP_ODFA_Requirements_Check( __FILE__ );
+
+$requirements->maybe_load();
